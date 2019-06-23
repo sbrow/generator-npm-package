8201e80de3241f92d3b65ef981c9b8520b831e4c
Spencer Brower <foxtrotalphalima@gmail.com>
2019-06-23 18:26:07 -0400

    feat: Created `prettier` generator.

---
+++ __tests__/prettier.test.ts
@@ -0,0 +1,76 @@
+import { writeFileSync } from "fs";
+import { join } from "path";
+import { ls } from "shelljs";
+import assert from "yeoman-assert";
+import { run, RunContextSettings } from "yeoman-test";
+
+import { load, loadJSON } from "../__setup__/fs";
+
+const appName = "prettier";
+
+let opts: RunContextSettings;
+let srcDir: string;
+let app: string;
+let prettier: string;
+
+beforeAll(() => {
+    opts = {
+        tmpdir: true,
+    };
+    srcDir = join(__dirname, "../src");
+    app = join(srcDir, appName);
+});
+
+beforeEach(() => {
+    prettier = "@sbrow/prettier-config";
+});
+
+describe(`generator-${appName}`, () => {
+    describe("Without input", () => {
+        it("should configure package.json:prettier = default", async () => {
+            const tmpDir = await run(app, opts).withPrompts({
+                configPackage: prettier,
+            });
+            const packageJson = loadJSON(tmpDir, "package.json");
+            expect(packageJson).toMatchObject({ prettier });
+        });
+        describe("Installer", () => {
+            let tmpDir: string;
+            let got: string;
+
+            beforeAll(async () => {
+                tmpDir = await run(app, opts).withOptions({
+                    "skip-install": false,
+                });
+                got = loadJSON(tmpDir, "package.json");
+            }, 15000);
+            it.each(["prettier", "@sbrow/prettier-config"])(
+                "should install %p",
+                (packageName: string) => {
+                    const want = {
+                        devDependencies: {},
+                    };
+                    want.devDependencies[packageName] = expect.any(String);
+                    expect(got).toMatchObject(want);
+                },
+            );
+        });
+    });
+    describe("With input", () => {
+        beforeEach(() => {
+            prettier = prettier.replace(/^@.*\//, "sbrower");
+        });
+        it("should configure package.json:prettier = prompt", async () => {
+            const tmpDir = await run(app, opts).withPrompts({
+                configPackage: prettier,
+            });
+            const packageJson = loadJSON(tmpDir, "package.json");
+            expect(packageJson).toMatchObject({ prettier });
+        });
+        it("should configure package.json:prettier = argument", async () => {
+            const tmpDir = await run(app, opts).withArguments([prettier]);
+            const packageJson = loadJSON(tmpDir, "package.json");
+            expect(packageJson).toMatchObject({ prettier });
+        });
+    });
+});
---
+++ src/prettier/index.ts
@@ -0,0 +1,56 @@
+import Generator from "yeoman-generator";
+
+import { BaseGenerator, BaseGeneratorOptions } from "../BaseGenerator";
+
+export interface PrettierOptions extends BaseGeneratorOptions {
+    prettier?: string;
+}
+
+/**
+ * Installs and configures {@link prettier}.
+ */
+export class PrettierGenerator extends BaseGenerator {
+    public options: PrettierOptions;
+
+    constructor(args: string | any[], opts: PrettierOptions) {
+        super(args, opts);
+        this.addDevDependencies("prettier");
+        this.argument("prettier", { type: String, required: false });
+    }
+
+    public async prompting() {
+        const prompts: Generator.Questions = [];
+        let username = "<username>";
+        try {
+            const name = await this.user.github.username();
+            username = name;
+        } catch (err) {
+            this.log(err, "error");
+        }
+        if (this.options.prettier === undefined) {
+            prompts.push({
+                type: "input",
+                name: "configPackage",
+                message: "Enter your package name:",
+                default: `@${username}/prettier-config`,
+                store: true,
+            });
+        }
+
+        const answers = await this.prompt(prompts);
+        if (this.options.prettier === undefined && answers.configPackage) {
+            this.options.prettier = answers.configPackage;
+        }
+        this.addDevDependencies(this.options.prettier);
+        this.scheduleInstall();
+    }
+
+    public writing() {
+        this.fs.extendJSON(this.destinationPath("package.json"), {
+            prettier: this.options.prettier,
+        });
+    }
+}
+
+module.exports = PrettierGenerator;
+export default PrettierGenerator;
