c01c9a494d248d4edf29944f0dc881fe662c2ca9
Spencer Brower <foxtrotalphalima@gmail.com>
2019-07-02 00:23:58 -0400

    test(Typescript): Added installation test.

--- __tests__/typescript.test.ts
+++ __tests__/typescript.test.ts
@@ -2,10 +2,10 @@ import { join } from "path";
 import { run } from "yeoman-test";
 
 import { loadJSON } from "../src/fs";
-import Typescript from "../src/typescript";
 
+let app: string;
 let opts;
-let srcDir;
+let srcDir: string;
 
 beforeAll(() => {
     opts = {
@@ -14,18 +14,20 @@ beforeAll(() => {
         namespace: "npm-package:typescript",
     };
     srcDir = join(__dirname, "../src");
+    app = join(srcDir, "typescript");
 });
+
 describe("generator-typescript", () => {
     describe("With default options", () => {
         it.skip(`Shows proper types`, async () => {
-            const context = run(Typescript, opts).withLocalConfig({
+            const context = run(app, opts).withLocalConfig({
                 dependencies: ["react"],
             });
         });
     });
     describe("When installed with React", () => {
         it('sets "jsx" to "react"', async () => {
-            const context = run(Typescript, opts).withLocalConfig({
+            const context = run(app, opts).withLocalConfig({
                 dependencies: ["react"],
             });
             const tmpDir = await context;
@@ -36,6 +38,20 @@ describe("generator-typescript", () => {
             };
             const got = loadJSON(tmpDir, "tsconfig.json");
             expect(got).toMatchObject(want);
+        });
+    });
+
+    it("Installs typescript", async () => {
+        const tmpDir = await run(app, opts);
+        const got = loadJSON(tmpDir, ".yo-rc.json");
+        expect(got).toMatchObject({
+            "generator-npm-package": {
+                devDependencies: expect.arrayContaining([
+                    "typescript",
+                    "@types/node",
+                    "tslint",
+                ]),
+            },
         });
     });
 });
