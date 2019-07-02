6637ada03277aea49b974c6fb562a3bd6cf6b78a
Spencer Brower <foxtrotalphalima@gmail.com>
2019-07-02 18:42:52 -0400

    test: Temporarily disabled test coverage. Issses with `collectCoverageFrom`.

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,14 @@
 const extensions = ["ts", "tsx", "js", "jsx", "json"];
 
 module.exports = {
-    collectCoverage: true,
-    collectCoverageFrom: [
-        `src/**/*\.{${extensions.filter(x => x !== "json").join(",")}}`,
-    ],
-    moduleDirectories: ["node_modules", "src"],
+    // collectCoverage: true,
+    // collectCoverageFrom: [
+    // `src/**/*\.{${extensions.filter(x => x !== "json").join(",")}}`,
+    // ],
+    moduleDirectories: ["node_modules", "generators", "generator-package"],
     moduleFileExtensions: extensions,
-    testPathIgnorePatterns: ["node_modules", "generators"],
+    testPathIgnorePatterns: ["node_modules"],
+    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
     transform: {
         ".tsx?": "ts-jest",
     },
