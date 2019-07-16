70fa6f75be58549185956922ecdaa11a119b612e
Spencer Brower <foxtrotalphalima@gmail.com>
2019-07-16 16:35:45 -0400

    build(jest.config): Updated to reflect new package structure.

--- jest.config.js
+++ jest.config.js
@@ -2,17 +2,13 @@ const extensions = ["ts", "tsx", "js", "jsx", "json"];
 
 module.exports = {
     collectCoverage: true,
-    collectCoverageFrom: [
-        "generator{s,-package,-prettier}/**/*.{ts,tsx}",
-        "!generator{s,-package,-prettier}/**/*.{d,test}*.ts",
-        "!**/templates/*",
-    ],
+    collectCoverageFrom: ["packages/**/*.{ts,tsx}"],
     globals: {
         "ts-jest": {
             diagnostics: { warnOnly: true },
         },
     },
-    moduleDirectories: ["node_modules", "generators", "generator-package"],
+    moduleDirectories: ["node_modules", "packages"],
     moduleFileExtensions: extensions,
     testPathIgnorePatterns: ["node_modules"],
     testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
